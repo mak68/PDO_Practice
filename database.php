aadbb8099f30c871abe895761e93e32114653d1e
Milena A Kosc <mak68@afsconnect1.njit.edu>
2017-11-05 14:17:53 -0500

    Added database.php

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+	$servername = "sql2.njit.edu";
+	$username = "mak68";
+	$password ="AEZ4mNaJP";
+	$dbname = "mak68";	
+	
+	
+try {
+	$conn = new PDO ("mysql:host = $servername; dbname=$dbname", $username, $password);
+	 
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected Successfully <br>";
+	}
+	
+catch (PDOException $e) {
+	echo "Connection failed: " . $e-> getMessage();
+	}
+
+?>
