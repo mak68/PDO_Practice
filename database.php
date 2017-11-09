82701e74989ad3894525a94f6654bf6b24b55caf
Milena A Kosc <mak68@afsconnect1.njit.edu>
2017-11-08 21:15:52 -0500

    final version

--- database.php
+++ database.php
@@ -6,12 +6,22 @@ $password = 'AEZ4mNaJP';
 try {
 
     $dbh = new PDO($dsn, $user, $password);
-       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "Connected successfully";
+ //      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected successfully<br>";
 } 
 
 	catch (PDOException $e) {
   	echo 'Connection failed: ' . $e->getMessage();
 	  }
+
+
+//$query = "SELECT * FROM accounts WHERE id = 6";
+//$statement = $db->prepare($query);
+//$statement->execute();
+//$results= $statement->fetchAll();
+//$statement->closeCursor();
+//$acct = $statement-> rowCount();
+//echo "Results";
+
 
 ?>
